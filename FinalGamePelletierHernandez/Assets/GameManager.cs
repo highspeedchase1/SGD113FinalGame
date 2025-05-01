c34e7b0ccf73f8daba3a1bb5620aa8843cde12b1
highspeedchase1 <cgpelletierhernandez@my.waketech.edu>
2025-05-01 16:45:29 -0400

    Part 2 Update
    Created Main Menu scene and UI. Updated the movement between scenes. Created a timer for invincibility.

--- FinalGamePelletierHernandez/Assets/GameManager.cs
+++ FinalGamePelletierHernandez/Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,14 +12,21 @@ public class GameManager : MonoBehaviour
 
     public int coins = 0;
 
+    float startCountdownValue = 10f;
+
     //UI Elements
+    public GameObject mainMenuPanel;
     public GameObject gameOverPanel;
     public GameObject youWinPanel;
+    public GameObject pickupTextGroup;
+    public GameObject invincibleCountdownParent;
     public TMP_Text redPotionsText;
     public TMP_Text bluePotionsText;
     public TMP_Text greenPotionsText;
     public TMP_Text coinsText;
     public TMP_Text healthText;
+    public TMP_Text invincibleCountdownText;
+
 
     private void Awake()
     {
@@ -34,41 +42,90 @@ public class GameManager : MonoBehaviour
     }
 
     private void Start()
-    {
-        //Hides UI 
+    { 
+        //Hides In Game UI 
         gameOverPanel.SetActive(false);
         youWinPanel.SetActive(false);
-
-        //Hides Curser
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        pickupTextGroup.SetActive(false);
     }
 
     public void HandleGameOver()
     {
         gameOverPanel.SetActive(true);
 
+        //Reactivates Cursor
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
 
     public void CheckLevelWin()
     {
-        if(coins == 10)
+        if(coins == 1)
         {
             youWinPanel.SetActive(true);
+
+            coins = 0;
+
+            //Reactivates Cursor
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
     
     public void Restart()
     {
-        //Reloads Current Scene
+        //Hides In Game UI 
+        gameOverPanel.SetActive(false);
+        youWinPanel.SetActive(false);
+        pickupTextGroup.SetActive(false);
+
+        //makes Main Menu visable again
+        mainMenuPanel.SetActive(true);
+
+        //Reactivates Cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void StartGame()
+    {
+        //Activates all of the in game UI except for invincibility countdown
+        pickupTextGroup.SetActive(true);
+        invincibleCountdownParent.SetActive(false);
+
+        //Disables Main Menu UI
+        mainMenuPanel.SetActive(false);
+
+        //Hides Curser
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         SceneManager.LoadScene("LevelOne", LoadSceneMode.Single);
     }
 
     public void GoToNextLevel()
     {
-        Debug.Log("Next level");
+        //Hides Curser
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        //Hides you win panel again
+        youWinPanel.SetActive(false);
+
+        //Get next scene index from build profile
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        //Checks to see if there are any more levels and if it is the last one, sends you back to the main menu
+        if(SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Restart();
+        }
     }
 
     public void EndGame()
@@ -86,6 +143,27 @@ public class GameManager : MonoBehaviour
         bluePotionsText.text = "Blue Potions: " + potions[2];
         coinsText.text = "Coins: " + coins;
         healthText.text = "Health: " + hp;
+    }
+
+    public void StartInvincibility()
+    {
+        invincibleCountdownParent.SetActive(true);
+        StartCoroutine(InvincibleCountdown(startCountdownValue));
+    }
+
+    IEnumerator InvincibleCountdown(float countdownValue)
+    {
+        startCountdownValue = countdownValue;
+        while(countdownValue > 0)
+        {
+            invincibleCountdownText.text = "Invincibility Left: " + countdownValue + " seconds";
+            yield return new WaitForSeconds(1f);
+            countdownValue--;
+        }
+        if(countdownValue == 0)
+        {
+            invincibleCountdownParent.SetActive(false);
+        }
     }
 
 }
